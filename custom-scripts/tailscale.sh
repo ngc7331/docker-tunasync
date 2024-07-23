71bc96b53275513e21803bbc76dc2bc7930d117a
ngc7331 <ngc7331@outlook.com>
2024-07-23 13:39:43 +0800

    feat(scripts/tailscale): modify default config

--- custom-scripts/tailscale.sh
+++ custom-scripts/tailscale.sh
@@ -5,17 +5,23 @@ set -o pipefail
 _here=`dirname $(realpath $0)`
 apt_sync="${_here}/apt-sync.py"
 
-ARCH="amd64,arm64,riscv64"
+ARCH="all,amd64,arm64,riscv64"
 VERSION="stable"
 BASE_URL="${TUNASYNC_UPSTREAM_URL:-https://pkgs.tailscale.com}/${VERSION}"
 BASE_PATH="${TUNASYNC_WORKING_DIR}/${VERSION}"
 
+# refer to https://github.com/tuna/tunasync-scripts/blob/c2051ee938594b22423f6d18e92690c9b11763fa/apt-sync.py#L31
+# currently, "@ubuntu-lts" = "focal,jammy,nobel"
+UBUNTU_CODENAME="@ubuntu-lts"
+# currently, "@debian-current" = "bullseye,bookworm"
+DEBIAN_CODENAME="@debian-current"
+
 # Ubuntu
-"$apt_sync" --delete "${BASE_URL}/ubuntu" @ubuntu-lts main ${ARCH} "${BASE_PATH}/ubuntu"
+"$apt_sync" --delete "${BASE_URL}/ubuntu" ${UBUNTU_CODENAME} main ${ARCH} "${BASE_PATH}/ubuntu"
 echo "Ubuntu finished"
 
 # Debian
-"$apt_sync" --delete "${BASE_URL}/debian" @debian-current main ${ARCH} "${BASE_PATH}/debian"
+"$apt_sync" --delete "${BASE_URL}/debian" ${DEBIAN_CODENAME} main ${ARCH} "${BASE_PATH}/debian"
 echo "Debian finished"
 
 # Done
